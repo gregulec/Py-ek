1903d5063b43912306ee92e1c4cf802dce41b4e7
Agnieszka Gregulec <agnieszka.gregulec@gmail.com>
2022-01-26 20:01:01 +0100

    upload

---
+++ Pyłek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyłek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Audi();
+            car = new Air(car);
+            Console.WriteLine(car.description());
+            car = new HotSits(car);
+            Console.WriteLine(car.description());
+            Console.ReadLine();
+        }
+    }
+}
